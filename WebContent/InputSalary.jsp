03a24aaaab4aaacb162e93d1f071976182d39840
Tejas <tejas14all@gmail.com>
2021-08-28 16:45:14 +0530

    validation and prefill

--- WebContent/InputSalary.jsp
+++ WebContent/InputSalary.jsp
@@ -6,12 +6,21 @@
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 </head>
 <body>
+	<%
+		String userNameValue = (String) request.getAttribute("userNameValue");
+		String bsValue = (String) request.getAttribute("bsValue");
+
+		String userError = (String) request.getAttribute("userNameError");
+		String bsError = (String) request.getAttribute("bsError");
+	%>
+
+
 	<form action="CalculateSalaryServlet">
-		Username : <input type="text" name="userName" /><br> 
-		BasicSalary:
-		<input type="text" name="bs" /><br>
-		 <input type="submit"
-			value="CalculateSalary" />
+		Username : <input type="text" name="userName" value="<%=userNameValue==null?"":userNameValue %>" />
+		<%=userError == null ? "" : userError%>
+		<br> BasicSalary: <input type="text" name="bs" value="<%=bsValue==null?"":bsValue %>" />
+		<%=bsError == null ? "" : bsError%>
+		<br> <input type="submit" value="CalculateSalary" />
 	</form>
 
 </body>
--- src/com/controller/CalculateSalaryServlet.java
+++ src/com/controller/CalculateSalaryServlet.java
@@ -14,23 +14,39 @@ public class CalculateSalaryServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		String userName = request.getParameter("userName");
-		String bs = request.getParameter("bs");// "20000"
+		String bs = request.getParameter("bs");// ""
 
-		int basicSalary = Integer.parseInt(bs);// 20000
-		int bonus = (basicSalary * 10) / 100;
-		int pf = (int) (basicSalary * 0.12);
-		int totalSalary = basicSalary + bonus - pf;
-	
-		
-		request.setAttribute("bs", basicSalary);
-		request.setAttribute("bn", bonus);
-		request.setAttribute("pf", pf);
-		request.setAttribute("ts", totalSalary);
-		request.setAttribute("un", userName);
-		
-		
-		
-		RequestDispatcher rd = request.getRequestDispatcher("PrintSalary.jsp");
+		boolean isError = false;
+
+		// validate
+		if (userName == null || userName.trim().length() == 0) {
+			isError = true;
+			request.setAttribute("userNameError", "Please Enter Username");
+		}else {
+			request.setAttribute("userNameValue",userName);
+		}
+
+		if (bs == null || bs.trim().length() == 0) {
+			isError = true;
+			request.setAttribute("bsError", "Please Enter Basic Salary");
+		}else {
+			request.setAttribute("bsValue", bs);
+		}
+		RequestDispatcher rd = null;
+		if (isError) {
+			rd = request.getRequestDispatcher("InputSalary.jsp");
+		} else {
+			int basicSalary = Integer.parseInt(bs);// ""
+			int bonus = (basicSalary * 10) / 100;
+			int pf = (int) (basicSalary * 0.12);
+			int totalSalary = basicSalary + bonus - pf;
+			request.setAttribute("bs", basicSalary);
+			request.setAttribute("bn", bonus);
+			request.setAttribute("pf", pf);
+			request.setAttribute("ts", totalSalary);
+			request.setAttribute("un", userName);
+			rd = request.getRequestDispatcher("PrintSalary.jsp");
+		}
 		rd.forward(request, response);
 	}
 }

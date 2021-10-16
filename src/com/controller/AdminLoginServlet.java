4549d2db85504c6c5abd2cb766330d192d1ebaea
Tejas <tejas14all@gmail.com>
2021-10-16 16:30:11 +0530

    init param - context param

--- src/com/controller/AdminLoginServlet.java
+++ src/com/controller/AdminLoginServlet.java
@@ -3,26 +3,44 @@ package com.controller;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/AdminLoginServlet")
 public class AdminLoginServlet extends HttpServlet {
+
+	String adminEmail  = "";
+	String adminPassword = "";
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+
+		adminEmail  = config.getInitParameter("email");
+		adminPassword = config.getInitParameter("password");
+	}
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		RequestDispatcher rd = null;
-		if (email.equals("admin@admin.com") && password.equals("admin")) {
+	
+		ServletContext context = getServletContext();
+		
+		System.out.println("Global Email => "+context.getInitParameter("globalEmail"));
+		System.out.println("Global Password => "+context.getInitParameter("globalPassword"));
+		
+		
+		
+		System.out.println("admin email password"+adminEmail+" "+adminPassword);
+		if (email.equals(adminEmail) && password.equals(adminPassword)) {
 
 			rd = request.getRequestDispatcher("AdminDashboard.jsp");
-	 	} else {
-	 		request.setAttribute("error", "Invalid Credentials");
-	 		rd = request.getRequestDispatcher("AdminLogin.jsp");
+		} else {
+			request.setAttribute("error", "Invalid Credentials");
+			rd = request.getRequestDispatcher("AdminLogin.jsp");
 		}
 		rd.forward(request, response);
 
